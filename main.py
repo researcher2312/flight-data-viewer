07a988058ed91e1b20bfede7541c246d43b7b48c
researcher2312 <researcher2312@gmail.com>
2023-01-04 14:41:38 +0100

    changed geometry

--- main.py
+++ main.py
@@ -9,13 +9,14 @@ class App(cti.CTk):
     def __init__(self):
         super().__init__()
 
-        self.geometry("600x300")
+        self.geometry("800x400")
         self.title("Flight data viewer 0.1")
         self.minsize(600, 200)
+        self.protocol("WM_DELETE_WINDOW", self.close_application)
 
         self.grid_rowconfigure(0, weight=1)
-        self.grid_columnconfigure((1, 2, 3), weight=0)
-        self.grid_columnconfigure((0, 4), weight=1)
+        self.grid_columnconfigure((1, 2, 3, 4), weight=0)
+        self.grid_columnconfigure((0, 5), weight=1)
 
         x = np.linspace(0.0, 5.0, 501)
 
@@ -28,21 +29,38 @@ class App(cti.CTk):
         ax2.set_xlabel('time (s)')
 
         self.chart = FigureCanvasTkAgg(fig, master=self)
-        self.chart.get_tk_widget().grid(row=0, column=0, columnspan=5, padx=20, pady=(20, 0), sticky="nsew")
+        self.chart.get_tk_widget().grid(row=0, column=0, columnspan=6, padx=20, pady=(20, 0), sticky="nsew")
 
         self.frame = cti.CTkLabel(master=self)
-        self.frame.grid(row=1, column=0, columnspan=5, padx=20, pady=(20, 0), sticky="nsew")
+        self.frame.grid(row=1, column=0, columnspan=6, padx=20, pady=(20, 0), sticky="nsew")
 
-        self.combobox = cti.CTkOptionMenu(master=self, values=["Sample text 1", "Text 2"])
-        self.combobox.grid(row=2, column=1, padx=20, pady=20, sticky="ew")
-        self.buttonConnect = cti.CTkButton(master=self, command=self.button_callback, text="Connect")
-        self.buttonConnect.grid(row=2, column=2, padx=20, pady=20, sticky="ew")
-        self.buttonRecord = cti.CTkButton(master=self, command=self.button_callback, text="Record")
-        self.buttonRecord.grid(row=2, column=3, padx=20, pady=20, sticky="ew")
+        self.combobox = cti.CTkOptionMenu(master=self, command=self.option_menu_callback, values=['no devices'])
+        self.combobox.grid(row=2, column=1, padx=10, pady=20, sticky="ew")
 
-    def button_callback(self):
-        self.bar1.plot(2,2)
-        self.bar1.draw()
+        self.button_search = cti.CTkButton(master=self, command=self.search_button_callback, text="Search")
+        self.button_search.grid(row=2, column=2, padx=10, pady=20, sticky="ew")
+
+        self.buttonConnect = cti.CTkButton(master=self, command=self.connect_button_callback, text="Connect")
+        self.buttonConnect.grid(row=2, column=3, padx=10, pady=20, sticky="ew")
+
+        self.buttonRecord = cti.CTkButton(master=self, command=self.record_button_callback, text="Record")
+        self.buttonRecord.grid(row=2, column=4, padx=10, pady=20, sticky="ew")
+
+    def option_menu_callback(self, choice):
+        print(choice)
+
+    def search_button_callback(self):
+        self.combobox.configure(values=['1', '2'])
+
+    def connect_button_callback(self):
+        pass
+
+    def record_button_callback(self):
+        pass
+
+    def close_application(self):
+        self.destroy()
+        quit()
 
 
 if __name__ == "__main__":
