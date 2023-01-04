ab8fa07252d8a9b651d6c6fdf1f2ac60bc3b3a9a
researcher2312 <researcher2312@gmail.com>
2023-01-04 16:27:30 +0100

    formatted the code with black

--- bluetooth_receiver.py
+++ bluetooth_receiver.py
@@ -1,6 +1,7 @@
 import bluetooth
 
-class BluetoothReceiver():
+
+class BluetoothReceiver:
     def __init__(self):
         self.available_devices = []
         self.connected_device = None
@@ -10,16 +11,16 @@ class BluetoothReceiver():
         try:
             self.available_devices = bluetooth.discover_devices()
         except OSError:
-            return("No bluetooth hardware")
+            return "No bluetooth hardware"
         except:
-            return("error")
+            return "error"
         else:
             for dev in self.available_devices:
-                print(f'{dev}: {bluetooth.lookup_name(dev)}')
+                print(f"{dev}: {bluetooth.lookup_name(dev)}")
                 services = bluetooth.find_service(address=dev)
                 for i in services:
                     print(i)
-    
+
     def connect_to_device(self):
         bd_addr = "01:23:45:67:89:AB"
         port = 0x1001
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 import bluetooth_receiver as bt
 
+
 class App(cti.CTk):
     def __init__(self):
         super().__init__()
@@ -22,29 +23,41 @@ class App(cti.CTk):
         x = np.linspace(0.0, 5.0, 501)
 
         fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
-        ax1.plot(x, np.cos(6*x) * np.exp(-x))
-        ax1.set_ylabel('acceleration')
+        ax1.plot(x, np.cos(6 * x) * np.exp(-x))
+        ax1.set_ylabel("acceleration")
 
-        ax2.plot(x, np.cos(6*x))
-        ax2.set_ylabel('rotation')
-        ax2.set_xlabel('time (s)')
+        ax2.plot(x, np.cos(6 * x))
+        ax2.set_ylabel("rotation")
+        ax2.set_xlabel("time (s)")
 
         self.chart = FigureCanvasTkAgg(fig, master=self)
-        self.chart.get_tk_widget().grid(row=0, column=0, columnspan=6, padx=20, pady=(20, 0), sticky="nsew")
+        self.chart.get_tk_widget().grid(
+            row=0, column=0, columnspan=6, padx=20, pady=(20, 0), sticky="nsew"
+        )
 
         self.label = cti.CTkLabel(master=self)
-        self.label.grid(row=1, column=0, columnspan=6, padx=20, pady=(20, 0), sticky="nsew")
+        self.label.grid(
+            row=1, column=0, columnspan=6, padx=20, pady=(20, 0), sticky="nsew"
+        )
 
-        self.combobox = cti.CTkOptionMenu(master=self, command=self.option_menu_callback, values=['no devices'])
+        self.combobox = cti.CTkOptionMenu(
+            master=self, command=self.option_menu_callback, values=["no devices"]
+        )
         self.combobox.grid(row=2, column=1, padx=10, pady=20, sticky="ew")
 
-        self.button_search = cti.CTkButton(master=self, command=self.search_button_callback, text="Search")
+        self.button_search = cti.CTkButton(
+            master=self, command=self.search_button_callback, text="Search"
+        )
         self.button_search.grid(row=2, column=2, padx=10, pady=20, sticky="ew")
 
-        self.buttonConnect = cti.CTkButton(master=self, command=self.connect_button_callback, text="Connect")
+        self.buttonConnect = cti.CTkButton(
+            master=self, command=self.connect_button_callback, text="Connect"
+        )
         self.buttonConnect.grid(row=2, column=3, padx=10, pady=20, sticky="ew")
 
-        self.buttonRecord = cti.CTkButton(master=self, command=self.record_button_callback, text="Record")
+        self.buttonRecord = cti.CTkButton(
+            master=self, command=self.record_button_callback, text="Record"
+        )
         self.buttonRecord.grid(row=2, column=4, padx=10, pady=20, sticky="ew")
 
         self.bluetooth_receiver = bt.BluetoothReceiver()
